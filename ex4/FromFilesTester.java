ea34e505d02ef673430c569d7f30387ff6064154
Ben Asaf <ToBenAsaf@Gmail.com>
2015-05-11 00:44:24 +0300

    Final version.

--- ex4/FromFilesTester.java
+++ ex4/FromFilesTester.java
@@ -14,22 +14,27 @@ public class FromFilesTester {
 
     private static AvlTree myTree;  // The general AvlTree type variable that the tester works with.
 
-    private static final String FULL_PATH = "tests/";
+    private static final String DEFAULT_PATH = "src\\oop\\ex4\\data_structures\\tests\\";   // DEFAULT PATH
 
     private static final Scanner userInput = new Scanner(System.in);
 
-    private static final int MAX_WORDS = 20;  // Maximum words per line. example: add 1 true (3). etc..
-    private static final int MAX_LINES = 20;  // Maximum lines of commands in a file.
+    private static boolean wasConstructedAlready = false;  // Do not touch.
+
+    private static final int MAX_WORDS = 50;  // Maximum words per line. example: add 1 true (3). etc..
+    private static final int MAX_LINES = 100;  // Maximum lines of commands in a file.
     public static String[][][] test = null;  // Files, Lines, Words - 3d array.
     private static String[] fileNames = null;
 
     // Commands used:
-    private static final String CONSTRUCT = "";
-    private static final String ADD = "add";
-    private static final String CONTAINS = "contains";
-    private static final String DELETE = "delete";
-    private static final String SIZE = "size";
-    private static final String MIN_NODES = "minNodes";
+    private static final String DATA_CONSTRUCT = "data";  // oop format
+    private static final String CONSTRUCT = "";  // OOP format
+    private static final String CONSTRUCT2 = "c"; // Custom format
+    private static final String COPY = "copy"; // oop format
+    private static final String ADD = "add";  // oop
+    private static final String CONTAINS = "contains";  // oop
+    private static final String DELETE = "delete";  // oop
+    private static final String SIZE = "size";  // oop
+    private static final String MIN_NODES = "minNodes"; // oop
 
     // Some constants...
     private static final int CMD = 0;
@@ -50,14 +55,22 @@ public class FromFilesTester {
             File folder = new File(path);
             File[] filesList = folder.listFiles();
 
-            fileNames = new String[filesList.length];
-            test = new String[filesList.length][MAX_LINES][MAX_WORDS];
-            for (int file = 0; file<test.length; file++){
+            fileNames = new String[filesList.length];  // To display file names.
+
+            test = new String[filesList.length][MAX_LINES][MAX_WORDS];  // Initializing the test array.
+
+            for (int file = 0; file<test.length; file++){  // Iterating on files.
                 fileNames[file] = filesList[file].getName();
                 String[] lines = Ex3Utils.fileToLines(path+fileNames[file]);
-                for (int line = 0; line < lines.length; line++){
+                for (int line = 0; line < lines.length; line++){  // Iterating on lines in files.
+                    if (line == MAX_LINES){  // To PREVENT OUTOFARRAY BOUNDS
+                        break;
+                    }
                     String[] words = lines[line].split(" ");
-                    for (int word = 0; word < words.length; word++){
+                    for (int word = 0; word < words.length; word++){  // Iterating on words in lines in files
+                        if (word == MAX_WORDS){  // To PREVENT OUTOFARRAY BOUNDS
+                            break;
+                        }
                         test[file][line][word] = words[word];
                     }
                 }
@@ -79,12 +92,13 @@ public class FromFilesTester {
             values[0] = Integer.parseInt(line[VALUE]);
             values[1] = Integer.parseInt(line[RESULT]);
             if ( myTree.findMinNodes(values[0]) != values[1]){
-                printError("Failed at: 'findMinNodes'" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
+                printError("Failed at: 'findMinNodes'" + "\nFile: " + fileNames[currentFile] + "\nLine: " +
+                        currentLine + "\nExpected: " + values[1] + ", Actual: " +values[0]);
             }
         } catch (Exception e){
-            printError("Failed at: 'findMinNodes'" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
-        }
+            printError("EXCEPTION at: 'findMinNodes'" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
 
+        }
     }
 
     /**
@@ -92,16 +106,49 @@ public class FromFilesTester {
      *
      */
     private static void add(String[] line){
-        int value;
-        boolean result;
+        int value = 0;
+        boolean result = false;
         try {
             value = Integer.parseInt(line[VALUE]);
             result = Boolean.parseBoolean(line[RESULT]);
             if ( myTree.add(value) != result){
-                printError("Failed at 'add' method!\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
+                printError("EXCEPTION at 'add' method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " +
+                        currentLine + "\nExpected: " + result + ", Actual: " + value);
             }
         } catch (Exception e){
-            printError("Failed at 'add' method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
+            printError("EXCEPTION at 'add' method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine);
+
+        }
+    }
+
+    /**
+     * Runs the size method from the AvlTree.
+     *
+     * @param line - String[] from the current line working on from file.
+     */
+    private static void size(String[] line){
+        int actual = 0;
+        int result = 0;
+        try{
+            result = Integer.parseInt(line[VALUE]);
+            actual = myTree.size();
+            if (actual != result){
+                printError("Failed at: 'size'" + "\nFile: " + fileNames[currentFile] + "\nLine: " +
+                        currentLine + "\nExpected: " + result + ", Actual: " + actual);
+            }
+        } catch (Exception e){
+            printError("EXCEPTION at: 'size'" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine);
+        }
+    }
+
+    /**
+     * Copies an AVL Tree to another.
+     */
+    private static void copy(){
+        try{
+            AvlTree myTree2 = new AvlTree(myTree);
+        }catch (Exception e){
+            printError("EXCEPTION when trying to copy AVL Tree using constructor." + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine);
         }
     }
 
@@ -114,15 +161,16 @@ public class FromFilesTester {
             int counter=0;
             for (String blah: line){
                 if (blah == null) break;  // If null, no more numbers
+                else if (blah == DATA_CONSTRUCT) continue;  // if data cmd.
                 else counter++;
             }
             int[] values = new int[counter];  // values to construct with array
-            for (int num = 0; num<counter; num++){  //  Parsing Strings to ints.
-                values[num] = Integer.parseInt(line[num]);
+            for (int num = 1; num<counter; num++){  //  Parsing Strings to ints.
+                values[num-1] = Integer.parseInt(line[num]);
             }
             myTree = new AvlTree(values);
         }catch (Exception e){
-            printError("Failed to call data constructor." + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine);
+            printError("EXCEPTION at call to data constructor." + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine);
         }
     }
 
@@ -134,7 +182,7 @@ public class FromFilesTester {
         try{
             myTree = new AvlTree();
         }catch (Exception e){
-            printError("Failed to call default constructor." + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine);
+            printError("EXCEPTION at call to default constructor." + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine);
         }
     }
 
@@ -144,16 +192,20 @@ public class FromFilesTester {
      * @param line - String[] from the current line working on from file.
      */
     private static void contains(String[] line){
-        int value;
-        int result;
+        int value = 0;
+        int result = 0;
+        int actual = 0;
         try{
             value = Integer.parseInt(line[VALUE]);
             result = Integer.parseInt(line[RESULT]);
-            if ( myTree.contains(value) != result){
-                printError("Failed at contains method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
+            actual = myTree.contains(value);
+            if ( actual != result){
+                printError("Failed at contains method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " +
+                        currentLine + "\nExpected: " + result + ", Actual: " + actual);
+
             }
         } catch (Exception e){
-            printError("Failed at contains method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
+            printError("EXCEPTION at contains method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
         }
     }
 
@@ -163,16 +215,18 @@ public class FromFilesTester {
      * @param line - String[] from the current line working on from file.
      */
     private static void delete(String[] line){
-        int value;
-        boolean result;
+        int value = 0;
+        boolean result = false;
         try{
             value = Integer.parseInt(line[VALUE]);
             result = Boolean.parseBoolean(line[RESULT]);
             if ( myTree.delete(value) != result){
-                printError("Failed at deleted method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
+                printError("Failed at deleted method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " +
+                        currentLine + "\nExpected: " + result + ", Actual: " + value);
+
             }
         } catch (Exception e){
-            printError("Failed at deleted method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
+            printError("EXCEPTION at deleted method!" + "\nFile: " + fileNames[currentFile] + "\nLine: " + currentLine );
         }
     }
 
@@ -190,6 +244,7 @@ public class FromFilesTester {
         printThumbsDown();
         exit();
     }
+
 
     /**
      * Exits program.
@@ -241,7 +296,7 @@ public class FromFilesTester {
 
 
     public static void main(String args[]){
-        String path = "src\\oop\\ex4\\data_structures\\tests\\";
+        String path = DEFAULT_PATH;
         try{
             try{
                 File folder = new File(path);
@@ -253,6 +308,7 @@ public class FromFilesTester {
                 System.err.println("Couldn't find tests folder using: "+ "'" + path + "'");
                 path = takeInputFromUser();
             }
+            // STARTS HERE.
             getAllTestsFiles(path);  // Loads all files,lines and words.
             currentFile = 0;  // Counter to keep current file updated.
             currentLine = 1;
@@ -267,8 +323,13 @@ public class FromFilesTester {
                         else if (word.equals("#")){  // comment line - ignore.
                             break;
                         }
-                        else if (word.equals(CONSTRUCT)) {  // "" - Construct default
-                            constructDefault();
+                        else if (word.equals(CONSTRUCT) || word.equals(CONSTRUCT2)) {  // "" - Construct default
+                            if (!wasConstructedAlready){
+                                constructDefault();
+                                wasConstructedAlready = true;
+                            }
+                            else System.err.println("Warning: Tried to call constructor, or was it a blank line?" +
+                                    ". It's ok tho, chill.");
                         }
                         else if (word.equals(ADD)) {  // "add" keyword
                             add(line);
@@ -282,12 +343,26 @@ public class FromFilesTester {
                         else if (word.equals(CONTAINS)) {  // 'contains' keyword
                             contains(line);
                         }
+                        else if (word.equals(COPY)){  // copy avl tree
+                            copy();  // Takes previous built tree (myTree) and puts in myTree2.
+                        }
+                        else if (word.equals(DATA_CONSTRUCT)){  // CUSTOM COMMAND.
+                            if (!wasConstructedAlready){
+                                constructWithData(line);
+                                wasConstructedAlready = true;
+                            }
+                            else System.err.println("Warning: Tried to call constructor, or was it a blank line?" +
+                                    ". It's ok tho, chill.");
+                        }
+                        else if (word.equals(SIZE)){
+                            size(line);
+                        }
                         else{  // If none of them: data constructor. will try to parse the string into int.
-                            try{
+                            try{  // THIS PART IS FOR THE OOP CONSTRUCTION WITH DATA. ONLY INTS.
                                 int num = Integer.parseInt(word);  // If integer, will continue to next line
                                 constructWithData(line);  // It is integer, call data constructor.
                             } catch (NumberFormatException e) {  // Not integers, not data constructor.
-                                // not an integer! (data constructor)
+                                printError("\nNot a valid command! " + "'" + word + "'" + "\nIn line: " + line[currentLine]);
                             }
                         }
                         break;
@@ -297,6 +372,7 @@ public class FromFilesTester {
                 System.out.println("");  // Prepares for net current file print.
                 currentLine=1;  // Resets line.
                 currentFile++;  // Just to keep current file name updated..
+                wasConstructedAlready = false;
             }
         } catch(Exception e){
             printError("");
